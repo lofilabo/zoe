c6a1dcf5219576523fc71a666d5ef1a1cce48021
root <root@virtualdebian001>
2014-09-09 19:54:58 +0100

    remoned unwanted

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tbcdigital/Developer/lci-future/lci
-# Build directory: /Users/tbcdigital/Developer/lci-future/lci
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test)
